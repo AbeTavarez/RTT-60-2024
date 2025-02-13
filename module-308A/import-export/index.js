e6fccc5999e6108fe471b03fccceaa5f42b6cf30
Abraham E. Tavarez <abeincwireless@gmail.com>
2025-02-13 09:44:21 -0500

    import export

---
+++ module-308A/import-export/index.js
@@ -0,0 +1,10 @@
+import { add } from "./helpers.js";
+import { nums } from "./myModule.js";
+
+console.log(nums);
+
+nums.forEach(n => console.log(n*2));
+
+
+const r1 = add(2,2)
+console.log(r1);
---
+++ module-308A/import-export/myModule.js
@@ -0,0 +1,20 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // alert("hello")
+})
+
+const userNames = ["max", 'alice'];
+
+function multiply(n1, n2) {
+    return n1*n2
+}
+
+const result = multiply(2,5)
+console.log(result);
+
+export const nums = [1,2,3,4,5];
+
+
+export function greeting() {
+    console.log("Hello");
+    
+}
