0d0a422a755d5c2082e52e5b361af4f9c7bec762
Abraham E. Tavarez <abeincwireless@gmail.com>
2025-02-21 21:37:14 -0500

    First React App

---
+++ module-320/first-react-app/src/App.jsx
@@ -0,0 +1,17 @@
+import NavBar from './components/NavBar'
+import './App.css'
+
+
+function App() {
+
+  return (
+    <div>
+      <NavBar />
+      {/* This is a comment */}
+      <h1>First React App</h1>
+      <hr />
+    </div>
+  )
+}
+
+export default App
