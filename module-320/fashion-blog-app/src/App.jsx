b56ec7c0a25f04df8894f7e9df2234c443554223
Abraham E. Tavarez <abeincwireless@gmail.com>
2025-02-27 10:49:10 -0500

    Fashion Blog/Learners Data

--- module-320/fashion-blog-app/src/App.jsx
+++ module-320/fashion-blog-app/src/App.jsx
@@ -1,28 +1,34 @@
 import "./App.css";
-import Article from "./components/Article";
 import Learner from "./components/Learner";
 
-import learnerData from './data/learners';
+import learnerData from "./data/learners";
 
-import image from "./assets/blog-image-1.jpg";
-import image2 from "./assets/blog-image-1.jpg";
+import {useState} from 'react';
+import ShowCount from "./components/ShowCount";
 
 function App() {
+  const [count, setCount] = useState(0);
+
   return (
     <div>
       <h1>Fashion Blog</h1>
 
-    {
-      learnerData.map((item, index) => (
-        <Learner
-        key={index}
-        name={item.name}
-        age={item.age}
-        lastLogin={item.lastLogin}
-         />
-      ))
-    }
-      
+      <ShowCount count={count}/>
+
+      {/* {arrOfLearners} */}
+
+      {learnerData.map((item, index) => {
+        return (
+          <Learner
+            key={index}
+            name={item.name}
+            age={item.age}
+            lastLogin={item.lastLogin}
+            style={{backgroundColor: 'black', color: "#fff"}}
+            setCount={setCount}
+          />
+        );
+      })}
     </div>
   );
 }
--- module-320/fashion-blog-app/src/components/Learner.jsx
+++ module-320/fashion-blog-app/src/components/Learner.jsx
@@ -1,10 +1,14 @@
 /* eslint-disable react/prop-types */
 
+
 function Learner(props) {
   console.log(props);
 
+  props.setCount(100);
+  
+
   return (
-    <div>
+    <div style={props.style}>
       <h2>Name: {props.name}</h2>
 
       <h3>Age: {props.age}</h3>
