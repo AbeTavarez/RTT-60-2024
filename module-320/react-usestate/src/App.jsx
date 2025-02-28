ca3f5480cbe6eaf5b11a459ac8b2d85a9405e140
Abraham E. Tavarez <abeincwireless@gmail.com>
2025-02-28 09:37:34 -0500

    Simple React App/Products

--- module-320/react-usestate/src/App.jsx
+++ module-320/react-usestate/src/App.jsx
@@ -1,5 +1,6 @@
 
 import './App.css'
+import Counter from './components/Counter'
 
 function App() {
 
@@ -7,6 +8,8 @@ function App() {
   return (
     <>
      <h1>React useState</h1>
+
+     <Counter />
     </>
   )
 }
--- module-320/react-usestate/src/components/Counter.jsx
+++ module-320/react-usestate/src/components/Counter.jsx
@@ -1,7 +1,17 @@
+import { useState } from "react";
+
 function Counter() {
+    const [counter, setCounter] = useState(0);
+
+    const handleClick = () => {
+        setCounter(counter + 1)
+    }
+
     return (
         <div>
-            <h1>Counter</h1>
+            <h1>Count: {counter}</h1>
+            <hr/>
+            <button onClick={handleClick}>Add</button>
         </div>
     )
 }
