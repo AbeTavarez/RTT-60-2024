8970ce472d330d9bdb90807148de9591ea1c6e41
Abraham E. Tavarez <abeincwireless@gmail.com>
2025-03-07 20:35:40 -0500

    Minor CSS UI updates

--- module-320/chatbot/src/App.jsx
+++ module-320/chatbot/src/App.jsx
@@ -7,6 +7,10 @@ function App() {
   const [prompt, setPrompt] = useState("");
   const [messages, setMessages] = useState([]);
 
+  /**
+   * Handle Form Submit
+   * @param {*} e 
+   */
   const handleSubmit = async (e) => {
     e.preventDefault();
 
@@ -39,13 +43,14 @@ function App() {
 
       {/* CHATS SECTION  */}
       <section className="flex-1 border p-5 bg-neutral-800">
-        <h1 className="text-center text-xl font-bold">Chatbot</h1>
+        <h1 className="text-center text-xl font-bold text-orange-500">Chatbot</h1>
+
       </section>
 
 
-      <section className="flex flex-col flex-3 border p-5 bg-neutral-900">
-        <h2 className="mb-5 text-center text-xl font-bold">Chat</h2>
-        <div className="">
+      <section className="flex flex-col flex-3 border bg-neutral-900">
+        <h2 className="mb-5 text-center text-xl font-bold bg-neutral-800 p-3 w-full">Chat</h2>
+        <div className="p-5">
           <div className="">
             {messages.map((message, idx) => {
               return (
@@ -59,15 +64,16 @@ function App() {
         </div>
  
         
-        <form onSubmit={handleSubmit} className="flex justify-between w-full mt-auto">
+        <form onSubmit={handleSubmit} className="flex justify-between w-full mt-auto p-3">
           <input
             type="text"
-            className="border w-full p-1"
+            className="border w-full p-1 rounded-lg"
             placeholder="Prompt"
             value={prompt}
             onChange={(e) => setPrompt(e.target.value)}
+            required
           />
-          <input type="submit" className="ml-2 border p-1" />
+          <input type="submit" className="ml-2 border p-2 rounded-lg hover:bg-white hover:text-black hover:cursor-pointer" />
         </form>
       </section>
     </main>
