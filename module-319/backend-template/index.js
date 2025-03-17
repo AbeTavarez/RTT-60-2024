dd2be39bf16b55d14c335872928249fa66bc031e
Abraham E. Tavarez <abeincwireless@gmail.com>
2025-03-17 11:21:39 -0400

    backend template

--- module-319/backend-template/index.js
+++ module-319/backend-template/index.js
@@ -1,4 +1,11 @@
 import express from 'express';
+import morgan from 'morgan';
+import helmet from 'helmet';
+import dotenv from 'dotenv';
+import cors from 'cors';
+
+dotenv.config();
+// console.log(process.env.MONGODB_URI);
 
 
 const PORT = process.env.PORT || 4000;
@@ -13,8 +20,14 @@ app.set("view engine", "pug");
 
 // Middlewares
 app.use(express.static("./public"));
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(morgan("dev"));
+app.use(helmet());
+app.use(cors());
 
 
+// Routes
 app.get('/', (req, res) => {
     res.render("index");
 })
